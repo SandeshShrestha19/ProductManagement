8f4d6294f061c91089a9a87e66414f262408e641
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-09 16:55:09 +0545

    feat: add property for category

--- ProductManagement/Models/Category.cs
+++ ProductManagement/Models/Category.cs
@@ -6,7 +6,7 @@ namespace ProductManagement.Models
     {
         public int Id { get; set; }
         public required string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
     }
 }
