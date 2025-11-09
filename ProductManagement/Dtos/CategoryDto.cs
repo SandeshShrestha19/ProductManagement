dbb46539384a3d24bae5a2ef329590f31f9bfd6e
SandeshShrestha19 <sandeshbhuju11@gmail.com>
2025-11-09 16:56:10 +0545

    feat: add user input for category related operations

--- ProductManagement/Dtos/CategoryDto.cs
+++ ProductManagement/Dtos/CategoryDto.cs
@@ -4,9 +4,7 @@ namespace ProductManagement.Dtos
 {
     public class CategoryDto
     {
-        public int Id { get; set; }
         public required string Name { get; set; }
-        public string Description { get; set; }
-        public List<Product> Products { get; set; }
+        public string? Description { get; set; }
     }
 }
